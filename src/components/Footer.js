04eee6f0f85c6841bdad9a61a445a4c5e2c4d246
devturnip <ossig.anthony@gmail.com>
2021-02-13 23:31:12 +0900

    correct contact info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ const Footer = () => {
                 <span class="navbar-text">
                     <p id="ft-a" class="footer-text">
                     Indola Pharma Link Singapore Pte Ltd, 8 Burn Road #10-09 Trivex, Singapore 369977
-                        Tel : +65 66369007 Fax : +65 66369008, Susanty HP : +65 90478019
+                        Tel : +65 66369007 Fax : +65 66369008
                     Email : <a href="mailto: indolaoffice@gmail.com">indolaoffice@gmail.com</a> or
                         <a href="mailto: indola@singnet.com.sg"> indola@singnet.com.sg</a>
                     </p>
